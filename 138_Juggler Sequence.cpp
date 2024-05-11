fdd1dfaa42d5035d7bc0c0a61553fd8b81049724
Khushi Soni <80217210+khushisoni110@users.noreply.github.com>
2024-05-11 20:10:41 +0530

    Create 138_Juggler Sequence.cpp

---
+++ 138_Juggler Sequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    vector<long long> jugglerSequence(long long n) {
+        // code here
+        vector<long long> ans;
+        while(n!=1){
+            ans.push_back(n);
+            if(n%2){
+                n=pow(sqrt(n),3);
+            }
+            else{
+                n=sqrt(n);
+            }
+        }
+        ans.push_back(1);
+        return ans;
+    }
+};
