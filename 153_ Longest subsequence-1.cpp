a28bffdd8dbf4ea84875e9559d7910aa0f0d8762
Khushi Soni <80217210+khushisoni110@users.noreply.github.com>
2024-05-27 18:47:47 +0530

    Create 153_ Longest subsequence-1.cpp

---
+++ 153_ Longest subsequence-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int longestSubseq(int n, vector<int> &a) {
+        vector<int> b(n,1);
+        int ans=0;
+        if(n==0){
+            return 0;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(abs(a[i]-a[j])==1){
+                    b[i]=max(b[i],1+b[j]);
+                    ans=max(ans,b[i]);
+                }
+            }
+        }
+        return ans;
+    }
+};
