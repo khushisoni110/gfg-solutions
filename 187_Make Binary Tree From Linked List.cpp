44070c735d9fd54a05eb6ea3c83bd07a6d4f21c2
Khushi Soni <80217210+khushisoni110@users.noreply.github.com>
2024-07-01 22:16:00 +0530

    Create 187_Make Binary Tree From Linked List.cpp

---
+++ 187_Make Binary Tree From Linked List.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+   void convert(Node *h, TreeNode *&r) {
+        queue<TreeNode*> q;
+        TreeNode *t = NULL;
+        
+        r = new TreeNode(h->data);
+        q.push(r);
+        h = h->next;
+        while(h and q.size()){
+            t = q.front(), q.pop();
+            t->left = new TreeNode(h->data);
+            h = h->next;
+            q.push(t->left);
+            
+            if(h){
+                t->right = new TreeNode(h->data);
+                q.push(t->right);
+                h = h->next;
+            }
+        }
+    }
+};
